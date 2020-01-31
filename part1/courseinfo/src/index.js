fb76e269e1edc840d2517d1bd495d3385de284a0
Tasos Tsournos <tsournos.anastasios@gmail.com>
2020-01-31 16:44:31 +0100

    First commit

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) =>{
+    return(
+        <>
+        <h1>{props.prop1}</h1>
+        </>
+    )
+}
+
+const Part = (props) =>{
+    return(
+        <>
+        <p>{props.part} {props.exers} </p>
+        </>
+    )
+}
+
+const Content = (props) =>{
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+    return(
+        <>
+            <Part part = {part1} exers = {exercises1} />
+            <Part part = {part2} exers = {exercises2} />
+            <Part part = {part3} exers = {exercises3} />
+
+        </>
+    )
+}
+
+const Total = (props) =>{
+    return(
+        <>
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+        </>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const exercises1 = 10
+  const exercises2 = 7
+  const exercises3 = 14
+
+  return (
+    <div>
+        <Header prop1={course} />
+        <Content />
+        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
