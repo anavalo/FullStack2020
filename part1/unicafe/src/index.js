17a10ef885c31272e4af66669a64f94e26b37214
Tasos Tsournos <tsournos.anastasios@gmail.com>
2020-02-05 10:03:10 +0100

    Unicafe step1 complete

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,56 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = (props) =>{
+const {onClick, text} = props
+return (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)}
+
+
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () =>{
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () =>{
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () =>{
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>
+      give feedback
+      </h1>
+      <div>
+      <Button onClick={handleGoodClick} text='Good'/>
+      <Button onClick={handleNeutralClick} text='Neutral'/>
+      <Button onClick={handleBadClick} text='Bad'/>
+      </div>
+      <h1>Statistics</h1>
+      <div>
+        good {good}
+      </div>
+      <div>
+        neutral {neutral}
+      </div>
+      <div>
+        bad {bad}
+      </div>
+      
     </div>
   )
 }
