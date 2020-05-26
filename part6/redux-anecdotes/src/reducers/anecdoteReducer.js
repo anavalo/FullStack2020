1bb67341e414949c7392d5a130b6f9021bbb0c6e
tasos <tsournos.anastasios@gmail.com>
2020-05-26 14:35:48 +0200

    fetching anecdotes from json-server

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,23 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
 
 export const vote = (id) => {
   console.log('vote', id)
@@ -38,10 +19,16 @@ export const createAnecdote = (content) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
+  }
+}
+
+const reducer = (state = [], action) => {
   switch (action.type) {
+
     case 'INCREMENT':
       const id = action.data.id
       const ancedoteToChange = state.find(m=>m.id === id)
@@ -50,8 +37,12 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote =>
         anecdote.id !== id? anecdote : changedAnecdote)
+
     case 'CREATE_NEW':
       return [...state, action.data]
+
+    case 'INIT_ANECDOTES':
+      return action.data
 
     default:
       return state
